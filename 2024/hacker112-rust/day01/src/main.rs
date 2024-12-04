9b9f5e7cac8115de79f933e537984e1f230cb8a2
hacker112 <>
2024-12-04 20:22:14 +0100

    Day 1 part 1

--- 2024/hacker112-rust/day01/src/main.rs
+++ 2024/hacker112-rust/day01/src/main.rs
@@ -1,3 +1,45 @@
+use std::{
+    fs::File,
+    io::{self, BufRead},
+    path::Path,
+};
+
+fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+where
+    P: AsRef<Path>,
+{
+    let file = File::open(filename)?;
+    Ok(io::BufReader::new(file).lines())
+}
+
 fn main() {
-    println!("Hello, world!");
+    if let Ok(lines) = read_lines("./input") {
+        let (mut first, mut second) =
+            lines
+                .flatten()
+                .fold((Vec::<i32>::new(), Vec::<i32>::new()), |mut acc, line| {
+                    let numbers: Vec<_> = line
+                        .split_whitespace()
+                        .map(|n| n.parse::<i32>().unwrap())
+                        .collect();
+
+                    acc.0.push(numbers[0]);
+                    acc.1.push(numbers[1]);
+
+                    return acc;
+                });
+        first.sort();
+        second.sort();
+
+        let sum: u32 = first
+            .iter()
+            .zip(second.iter())
+            .map(|x| {
+                let distance = x.0.abs_diff(*x.1);
+                distance
+            })
+            .sum();
+
+        println!("part 1: {}", sum);
+    }
 }
