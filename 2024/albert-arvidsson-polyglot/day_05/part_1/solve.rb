63de47cb093706b061c923c4fe25811663f107f6
Albert Arvidsson <albert.arvidsson@gmail.com>
2024-12-06 06:37:43 +0100

    Update albert-arvidsson-polyglot

--- 2024/albert-arvidsson-polyglot/day_05/part_1/solve.rb
+++ 2024/albert-arvidsson-polyglot/day_05/part_1/solve.rb
@@ -1,18 +1,14 @@
 #!/usr/bin/env ruby
 
-rules, updates = File.read(ARGV[0]).split("\n\n")
+rules, updates = File.read(ARGV[0]).split("\n\n").map { |s| s.split("\n") }
+rules = rules.to_set
 
-rules = rules.split("\n").map { |line| line.split('|').map(&:to_i) }
-
-updates = updates.split("\n").map { |update| update.split(',').map(&:to_i) }
-
-manuals = updates.filter do |update|
+manuals = updates.map { |s| s.split(',') }.filter do |update|
   update == update.sort do |a, b|
-    next -1 if rules.find { |(a2, b2)| a2 == a && b2 == b }
-    next 1 if rules.find { |(a2, b2)| b2 == a && a2 == b }
+    next -1 if rules.include?("#{a}|#{b}")
 
-    0
+    rules.include?("#{b}|#{a}") ? 1 : 0
   end
 end
 
-pp(manuals.sum { |manual| manual[manual.size / 2] })
+pp(manuals.sum { |manual| manual[manual.size / 2].to_i })
--- 2024/albert-arvidsson-polyglot/day_05/part_2/solve.rb
+++ 2024/albert-arvidsson-polyglot/day_05/part_2/solve.rb
@@ -1,19 +1,15 @@
 #!/usr/bin/env ruby
 
-rules, updates = File.read(ARGV[0]).split("\n\n")
+rules, updates = File.read(ARGV[0]).split("\n\n").map { |s| s.split("\n") }
+rules = rules.to_set
 
-rules = rules.split("\n").map { |line| line.split('|').map(&:to_i) }
-
-updates = updates.split("\n").map { |update| update.split(',').map(&:to_i) }
-
-manuals = updates.map do |update|
+manuals = updates.map { |update| update.split(',') }.map do |update|
   sorted = update.sort do |a, b|
-    next -1 if rules.find { |(a2, b2)| a2 == a && b2 == b }
-    next 1 if rules.find { |(a2, b2)| b2 == a && a2 == b }
+    next -1 if rules.include?("#{a}|#{b}")
 
-    0
+    rules.include?("#{b}|#{a}") ? 1 : 0
   end
   sorted == update ? nil : sorted
 end.compact
 
-pp(manuals.sum { |manual| manual[manual.size / 2] })
+pp(manuals.sum { |manual| manual[manual.size / 2].to_i })
